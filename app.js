9250194dff46497c5ff4fe0384b1694d99ab9e3e
ThasneemAzeez <thasneemazeez2001@gmail.com>
2024-06-13 15:54:08 +0530

    token developed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const { blogmodel } = require("./models/blog")
 const bcrypt = require("bcryptjs")
+const jwt =require("jsonwebtoken")
+
 
 mongoose.connect("mongodb+srv://thasneemazeez:thasneem38@cluster0.uk9okno.mongodb.net/bogdb?retryWrites=true&w=majority&appName=Cluster0")
 
@@ -19,7 +21,7 @@ const generateHashedPassword = async (password) => {
 
 
 app.post("/signup", async (req, res) => {
-   let input=req.body
+    let input = req.body
     //by doing this our password willbe hashed
     let hashedPassword = await generateHashedPassword(input.password)
     console.log(hashedPassword)
@@ -27,7 +29,37 @@ app.post("/signup", async (req, res) => {
     input.password = hashedPassword
     let blog = new blogmodel(input)
     blog.save()
-    res.json({"status":"success"})
+    res.json({ "status": "success" })
+})
+
+app.post("/signin", (req, res) => {
+//checking email is correct or not if yes it will go to login page or user not found
+    let input = req.body
+    blogmodel.find({ "email": req.body.email }).then(
+        (response) => {
+            if (response.length > 0) {
+                let dbPassword = response[0].password
+                console.log(dbPassword)
+                //checking our password and crypted password same crypted always have different values to make it equal
+               bcrypt.compare(input.password,dbPassword,(error,isMatch)=>{
+                if (isMatch) {
+                    jwt.sign({email:input.email},"blog-app" ,{expiresIn:"1d"},(error,token)=>{
+                        if (error) {
+                            res.json({"status":"unable to create token"})
+                        } else {
+                            res.json({"status":"success","userId":response[0]._id,"token":token})
+                        }
+                    })
+                   
+                } else {
+                    res.json({"status":"inccorect password"})
+                }
+               })
+               
+            } else {
+                res.json({ "status": "user not found" })
+            }
+        })
 })
 
 
